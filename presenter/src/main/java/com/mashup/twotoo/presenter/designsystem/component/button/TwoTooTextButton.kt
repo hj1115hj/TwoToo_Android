5589445beb7de70e7cbcc11abe26201fc713b04a
hj1115hj <hyejin_one@naver.com>
2023-06-21 21:52:47 +0900

    refactor: 린트 적용 및 string xml에 추가

--- presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/button/TwoTooTextButton.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/button/TwoTooTextButton.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mashup.twotoo.presenter.R
 import com.mashup.twotoo.presenter.designsystem.theme.*
 
 @Composable
@@ -59,7 +61,7 @@ private fun TextButtonDisabledPreview() {
     Column(modifier = Modifier.fillMaxSize()) {
         Spacer(modifier = Modifier.weight(1.0f))
         TwoTooTextButton(
-            text = "인증하기",
+            text = stringResource(id = R.string.authenticate),
             enabled = false,
         ) {}
         Spacer(modifier = Modifier.height(10.dp))
